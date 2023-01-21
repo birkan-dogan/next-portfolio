b5565aee7f0278aefdcad541477a3c3386658f84
birkan <b.561birkan@gmail.com>
2023-01-21 21:31:05 +0300

    running project

---
+++ constants/data.js
@@ -0,0 +1,95 @@
+const userData = {
+  githubUsername: "birkan-dogan",
+  name: "Birkan Dogan",
+  designation: "Full-Stack Developer",
+  avatarUrl: "/avatar.png",
+  email: "b.561birkan@gmail.com",
+  phone: "+90 546 850 3938",
+  address: "Ankara, Turkey",
+  projects: [
+    {
+      title: "Fire-Blog App",
+      link: "https://fire-blog-gamma.vercel.app/",
+      imgUrl: "/projectImg/fireblog.jpg",
+    },
+    {
+      title: "Movie App",
+      link: "https://movie-app-liart-eta.vercel.app/",
+      imgUrl: "/projectImg/movie-app.jpg",
+    },
+    {
+      title: "Recipe App",
+      link: "https://recipe-app-with-styledcomponents.vercel.app/",
+      imgUrl: "/projectImg/recipe-app.jpg",
+    },
+    {
+      title: "Fire-Contact",
+      link: "https://firecontact-app-tau.vercel.app/",
+      imgUrl: "/projectImg/fire-contact.jpg",
+    },
+    {
+      title: "Weather App",
+      link: "https://weather-app-ten-taupe.vercel.app/",
+      imgUrl: "/projectImg/weather-app.jpg",
+    },
+    {
+      title: "Add Tutorial",
+      link: "https://tutorialapp-delta.vercel.app/",
+      imgUrl: "/projectImg/tutorial.jpg",
+    },
+    {
+      title: "Random User",
+      link: "https://randomusers-tableversion.netlify.app/",
+      imgUrl: "/projectImg/randomuser.jpg",
+    },
+    {
+      title: "GitHub API",
+      link: "https://axios-router-project-tau.vercel.app/",
+      imgUrl: "/projectImg/axios-router.jpg",
+    },
+    {
+      title: "Breaking Bad API",
+      link: "https://breakingbadapicast.netlify.app/",
+      imgUrl: "/projectImg/breakingbad.jpg",
+    },
+    {
+      title: "IOS Calculator",
+      link: "https://birkan-dogan.github.io/iosCalculator/",
+      imgUrl: "/projectImg/ios.jpg",
+    },
+    {
+      title: "ToDo List",
+      link: "https://birkan-dogan.github.io/toDoList/",
+      imgUrl: "/projectImg/toDoList.jpg",
+    },
+    {
+      title: "Guess The Number",
+      link: "https://holdthenumber.netlify.app/",
+      imgUrl: "/projectImg/guess.jpg",
+    },
+    {
+      title: "Sass portfolio",
+      link: "https://birkan-dogan.github.io/portfolio/index.html",
+      imgUrl: "/projectImg/bootstrap.jpg",
+    },
+  ],
+  about: {
+    title:
+      "I'm a software developer who loves building products and applications that touch the world in a good way",
+    description: [
+      `I've been developing full-stack application since I was 15 years old. I didn't know what full-stack meant at that time.`,
+      `After learning HTML and struggling with CSS, I came up with a brilliant idea of using bootstrap so that I don't have to style everything by myself and - for obvious reasons - if you knew bootstrap, you were cool.`,
+      `But now, I'm a legend. I've been coding in React, Bootstrap, Tailwind, Python, Django PostgreSQL programming language etc to name a few.`,
+    ],
+    currentProject: "React Admin Dashboard",
+    currentProjectUrl: "https://react-admin.com",
+  },
+  certificates: [],
+  resumeUrl: "",
+  socialLinks: {
+    linkedin: "https://www.linkedin.com/in/birkandogandeveloper/",
+    github: "https://github.com/birkan-dogan",
+  },
+};
+
+export default userData;
