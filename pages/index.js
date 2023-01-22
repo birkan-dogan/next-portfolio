1c440310390e63c80e6954bf64e15535609d3732
birkan <b.561birkan@gmail.com>
2023-01-22 15:16:08 +0300

    ContainerBlock and Navbar

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 import Head from "next/head";
 import Image from "next/image";
+import ContainerBlock from "../components/ContainerBlock";
 // import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
-      <h2>hello</h2>
+      <ContainerBlock
+        title="Birkan Dogan - Full-Stack Developer"
+        description="Portfolio site to display full-stack projects"
+      ></ContainerBlock>
     </div>
   );
 }
