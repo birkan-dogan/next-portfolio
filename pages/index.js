18b7fe4029205ce40e6b9fdd81ce594845eab42a
birkan <b.561birkan@gmail.com>
2023-01-23 13:00:33 +0300

    LatestCode component works

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,12 @@ import Image from "next/image";
 import ContainerBlock from "../components/ContainerBlock";
 import FavouriteProjects from "../components/FavouriteProjects";
 import Hero from "../components/Hero";
+import LatestCode from "../components/LatestCode";
+import userData from "../constants/data";
+import getLatestRepos from "../lib/getLatestRepos";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export default function Home({ repositories }) {
   return (
     <div>
       <ContainerBlock
@@ -14,7 +17,21 @@ export default function Home() {
       >
         <Hero />
         <FavouriteProjects />
+        <LatestCode repositories={repositories} />
       </ContainerBlock>
     </div>
   );
 }
+
+export const getServerSideProps = async () => {
+  let token = process.env.GITHUB_AUTH_TOKEN;
+
+  const repositories = await getLatestRepos(userData, token);
+  // console.log("REPOSITORIES", repositories);
+
+  return {
+    props: {
+      repositories,
+    },
+  };
+};
