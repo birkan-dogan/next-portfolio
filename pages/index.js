c8b40167c53bcbe6ab344b89ab247e85fe29ebac
birkan <b.561birkan@gmail.com>
2023-01-23 12:44:24 +0300

    Favourite Projects component is done

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import ContainerBlock from "../components/ContainerBlock";
+import FavouriteProjects from "../components/FavouriteProjects";
 import Hero from "../components/Hero";
 import styles from "../styles/Home.module.css";
 
@@ -12,6 +13,7 @@ export default function Home() {
         description="Portfolio site to display full-stack projects"
       >
         <Hero />
+        <FavouriteProjects />
       </ContainerBlock>
     </div>
   );
